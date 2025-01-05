35706689691f4ae535678e835cce27a2802e1f89
ayodeji akinola <ayodejiakinola007@gmail.com>
2025-01-05 03:16:29 +0100

    vgf

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), visualizer({
+    open: true, // Automatically open the report in the browser
+  }),],
+  build: {
+    chunkSizeWarningLimit: 1000, // Adjust this value as needed
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
